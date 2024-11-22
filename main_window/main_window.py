66ae28d18c00b879c722bce274f427baca9f2310
Elias Hawa <elias.hawa2468@gmail.com>
2024-11-21 21:45:18 -0500

    Rename widget to main_window Organize main window related files

--- main_window/main_window.py
+++ main_window/main_window.py
@@ -19,7 +19,7 @@ import packet_spec
 # You need to run the following command to generate the ui_form.py file
 #     pyside6-uic form.ui -o ui_form.py, or
 #     pyside2-uic form.ui -o ui_form.py
-from ui_form import Ui_Widget
+from .ui import Ui_Widget
 
 sim_is_running = False
 i = 0
