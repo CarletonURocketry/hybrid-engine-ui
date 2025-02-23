da1c63f5a8c2c1a2d6c674c7439544538edc5c9f
Elias Hawa <elias.hawa2468@gmail.com>
2025-02-23 14:26:52 -0500

    Fix data stream being out of sync

--- main_window/serial.py
+++ main_window/serial.py
@@ -43,9 +43,22 @@ def refresh_serial_button_handler(self: "MainWindow"):
  
 # Any data received should be handled here
 def serial_receive_data(self: "MainWindow"):
-    # code for sending data over serial will send a struct consisting of 24
-    # bytes. Qt slot for serial ready receive might receive this over multiple
-    # signals, make sure buffer contains at least 24 bytes, then read all 24 at once
+    # The arduino code for sending data over serial will send a struct consisting of 24
+    # bytes representing a data packet for the entire system. In other words, this
+    # data packet contains all sensor values. The Qt slot for recieiving serial data
+    # might be signaled multiple times when data is transmitted once. To mitigate this
+    # we ensure that there are at least 24 bytes available before we read and parse 
+    # the entire packet
+
+    # Another important thing is this while loop. Serial data is an unsynchronized
+    # constant data stream meaning that if we start reading, we won't know where we
+    # are in the data stream. This is important as the size of the data packet is always
+    # 24 bytes and if we were to read in the middle of the packet, we would be reading invalid
+    # data. To fix that, this will loop reads bytes until it reads "~" which indicates the beginning
+    # of a data packet 
+    while (self.serialPort.peek(1) != b'~'):
+      self.serialPort.read(1)
+
     if self.serialPort.bytesAvailable() >= 24:
       data = bytes(self.serialPort.read(24))
       parsed_data = packet_spec.parse_serial_packet(data, self.serialTimestamp)
