b6cb79aee6ed333fe7a76800275e4eb1d0621209
Elias Hawa <elias.hawa2468@gmail.com>
2025-05-05 15:04:28 -0400

    Move existing data logging functionality to logging.py

--- main_window/main_window.py
+++ main_window/main_window.py
@@ -1,5 +1,6 @@
 # This Python file uses the following encoding: utf-8
 from dataclasses import dataclass
+from pathlib import Path
 
 from PySide6.QtWidgets import QWidget, QLabel, QMessageBox
 from PySide6.QtCore import QTimer, Qt, QMutex, QPoint
@@ -79,7 +80,7 @@ class MainWindow(QWidget):
         process_data, turn_off_valve, turn_on_valve, decrease_heartbeat, reset_heartbeat_timeout
     from .recording_and_playback import recording_toggle_button_handler, \
         open_file_button_handler, display_previous_data
-    from .logging import save_to_file, write_to_log
+    from .logging import save_to_file, write_to_log, create_csv_log, write_to_csv_log
     from .config import load_config, save_config, add_pressure_threshold_handler, \
     add_temperature_threshold_handler, add_tank_mass_threshold_handler, add_engine_thrust_threshold_handler
 
@@ -236,7 +237,8 @@ class MainWindow(QWidget):
         #Connect toggle button for recording data
         self.ui.recordingToggleButton.toggled.connect(self.recording_toggle_button_handler)
         self.file_out = None
-        self.csv_fieldnames = ["t","m1","m2","p1","p2","p3","p4","t1","t2","t3","status"]
+        self.csv_dir = Path("data_csv")
+        self.csv_fieldnames = ["t","m1","m2","p1","p2","p3","p4","p5","p6","t1","t2","t3","t4","status"]
         self.csv_out = None
 
         # Init valve and sensor labels
