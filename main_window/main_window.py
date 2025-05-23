e4d784ca0e939ccf8e09d721b6ddd543f954123f
Elias Hawa <elias.hawa2468@gmail.com>
2025-05-23 13:03:15 -0400

    Update PID window

--- main_window/main_window.py
+++ main_window/main_window.py
@@ -65,8 +65,8 @@ class PIDWindow(QWidget):
         self.setFixedSize(self.width(), self.height())
         for i in range(1, 5):
             self.value_labels[f"p{i}"] = getattr(self.ui, f"p{i}ValLabel")
-        for i in range(1, 3):
-            self.value_labels[f"t{i}"] = getattr(self.ui, f"t{i}ValLabel")
+        # for i in range(1, 3):
+        #     self.value_labels[f"t{i}"] = getattr(self.ui, f"t{i}ValLabel")
 
 class MainWindow(QWidget):
     # Imports for MainWindow functionality. Helps split large file into
