6f7e947421d45e58cd6acf4af250e4d67dbeff93
Elias Hawa <elias.hawa2468@gmail.com>
2025-06-20 21:30:37 -0400

    Add silly windows icon workaround

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import sys
+import ctypes
+
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
+
 from main_window import MainWindow
 
 if __name__ == "__main__":
+    appid = 'CUInSpace.Hybrid' # arbitrary string
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(appid)
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("hampter.ico"))
     main_window = MainWindow()
+    main_window.setWindowIcon(QIcon("hampter.ico"))
     main_window.show()
     sys.exit(app.exec())
