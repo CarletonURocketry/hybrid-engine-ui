c77bcb1af46b9288a5a1f7ad59779fb6f60930d4
Elias Hawa <elias.hawa2468@gmail.com>
2025-07-07 16:33:04 -0400

    Fix default open valves not being saved with configuration Add error message when config isn't correctly saved

--- main_window/config.py
+++ main_window/config.py
@@ -7,6 +7,7 @@ be imported by main_window.py
 import json
 from typing import TYPE_CHECKING
 
+from PySide6.QtWidgets import QMessageBox
 from pyqtgraph import mkPen, InfiniteLine
 
 if TYPE_CHECKING:
@@ -18,27 +19,47 @@ def load_config(self: "MainWindow", config):
     self.config = json.load(config)
     self.ui.udpIpAddressInput.setText(self.config["multicast_options"]["address"])
     self.ui.udpPortInput.setText(self.config["multicast_options"]["port"])
+    self.points_used_for_average = self.config["points_used_for_average"]
+    self.ui.defaultOpenValvesList.addItems([str(valve) for valve in self.config["default_open_valves"]])
     self.ui.pressureThresholdList.addItems([str(marker) for marker in self.config["thresholds"]["pressure"]])
     self.ui.temperatureThresholdList.addItems([str(marker) for marker in self.config["thresholds"]["temperature"]])
     self.ui.tankMassThresholdList.addItems([str(marker) for marker in self.config["thresholds"]["tank_mass"]])
     self.ui.engineThrustThresholdList.addItems([str(marker) for marker in self.config["thresholds"]["engine_thrust"]])
-    self.points_used_for_average = self.config["points_used_for_average"]
     self.graph_range = self.config["graph_range"]
 
 def save_config(self: "MainWindow"):
-    new_config = {}
-    new_config["multicast_options"] = {}
-    new_config["multicast_options"]["address"] = self.ui.udpIpAddressInput.text()
-    new_config["multicast_options"]["port"] = self.ui.udpPortInput.text()
-    new_config["thresholds"] = {}
-    new_config["thresholds"]["pressure"] = [float(self.ui.pressureThresholdList.item(x).text()) for x in range(self.ui.pressureThresholdList.count())]
-    new_config["thresholds"]["temperature"] = [float(self.ui.temperatureThresholdList.item(x).text()) for x in range(self.ui.temperatureThresholdList.count())]
-    new_config["thresholds"]["tank_mass"] = [float(self.ui.tankMassThresholdList.item(x).text()) for x in range(self.ui.tankMassThresholdList.count())]
-    new_config["thresholds"]["engine_thrust"] = [float(self.ui.engineThrustThresholdList.item(x).text()) for x in range(self.ui.engineThrustThresholdList.count())]
-    new_config["points_used_for_average"] = self.points_used_for_average
-    new_config["graph_range"] = self.graph_range
-    with open('config.json', 'w') as config_file:
-        json.dump(new_config, config_file, indent=4)
+    try:
+        new_config = {}
+        new_config["multicast_options"] = {}
+        new_config["multicast_options"]["address"] = self.ui.udpIpAddressInput.text()
+        new_config["multicast_options"]["port"] = self.ui.udpPortInput.text()
+        new_config["points_used_for_average"] = self.points_used_for_average
+        new_config["default_open_valves"] = [int(self.ui.defaultOpenValvesList.item(x).text()) for x in range(self.ui.defaultOpenValvesList.count())]
+        new_config["thresholds"] = {}
+        new_config["thresholds"]["pressure"] = [float(self.ui.pressureThresholdList.item(x).text()) for x in range(self.ui.pressureThresholdList.count())]
+        new_config["thresholds"]["temperature"] = [float(self.ui.temperatureThresholdList.item(x).text()) for x in range(self.ui.temperatureThresholdList.count())]
+        new_config["thresholds"]["tank_mass"] = [float(self.ui.tankMassThresholdList.item(x).text()) for x in range(self.ui.tankMassThresholdList.count())]
+        new_config["thresholds"]["engine_thrust"] = [float(self.ui.engineThrustThresholdList.item(x).text()) for x in range(self.ui.engineThrustThresholdList.count())]
+        new_config["graph_range"] = self.graph_range
+        with open('config.json', 'w') as config_file:
+            json.dump(new_config, config_file, indent=2)
+        self.display_popup(QMessageBox.Icon.Information, "Configuration saved", "Saved configuration")
+        self.write_to_log("Saved configuration")
+    except Exception as e:
+        self.display_popup(QMessageBox.Icon.Warning, "Configuration save failed", "Could not save configuration")
+        self.write_to_log("Could not save configuration")
+
+def add_default_open_valve_handler(self: "MainWindow"):
+    try:
+        if self.ui.defaultOpenValvesList.currentRow() == -1:
+            new_valve = self.ui.defaultOpenValvesInput.text()
+            self.config["default_open_valves"].append(int(new_valve))
+            self.ui.defaultOpenValvesList.addItem(str(int(new_valve)))
+            self.ui.defaultOpenValvesInput.setText("")
+        else:
+            self.ui.defaultOpenValvesList.takeItem(self.ui.defaultOpenValvesList.currentRow())
+    except Exception as e:
+        self.display_popup(QMessageBox.Icon.Critical, "Action failed", f"Adding default open valve failed\n{str(e)}")
 
 def add_pressure_threshold_handler(self: "MainWindow"):
     if self.ui.pressureThresholdList.currentRow() == -1:
