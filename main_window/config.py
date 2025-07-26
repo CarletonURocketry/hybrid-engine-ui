857839cd8ffafd19713d2bcdaf15347d22b1d581
Elias Hawa <elias.hawa2468@gmail.com>
2025-07-26 12:37:40 -0400

    Start moving config to config manager class

--- main_window/config.py
+++ main_window/config.py
@@ -7,60 +7,137 @@ be imported by main_window.py
 import json
 from typing import TYPE_CHECKING
 
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QMessageBox, QPushButton
+from PySide6.QtCore import Qt, QObject, Signal
+from PySide6.QtWidgets import QMessageBox, QRadioButton
 from pyqtgraph import mkPen, InfiniteLine
 
 from .plot_info import PlotDataDisplayMode
 
 if TYPE_CHECKING:
-    from PySide6.QtWidgets import QPushButton
-
+    from PySide6.QtWidgets import QRadioButton
     from main_window import MainWindow
 
 inf_line_pen = mkPen("black", width=2, style=Qt.PenStyle.DashLine)
 
-def load_config(self: "MainWindow", config):
-    self.config = json.load(config)
+class ConfigManager(QObject):
+    popup_ready = Signal(str)
+    log_ready = Signal()
 
-    # SHOULD ALL BE DONE IN UI MANAGER
-    self.ui.udpIpAddressInput.setText(self.config["multicast_options"]["address"])
-    self.ui.udpPortInput.setText(self.config["multicast_options"]["port"])
-    
-    # self.points_used_for_average = self.config["sensor_and_valve_options"]["points_used_for_average"]
-    self.ui.defaultOpenValvesList.addItems([str(valve) for valve in self.config["sensor_and_valve_options"]["default_open_valves"]])
-    if self.config["sensor_and_valve_options"]["pid_diagram"] == "cold_flow":
-        self.ui.pidDiagramColdFlowOption.setChecked(True)
-    elif self.config["sensor_and_valve_options"]["pid_diagram"] == "static_fire":
-        self.ui.pidDiagramStaticFireOption.setChecked(True)
+    def __init__(self):
+        super().__init__()
 
-    if self.config["graph_options"]["pressure"]["data_display_mode"] == "points":
-        self.ui.pressureLastXPointsRB.setChecked(True)
-    elif self.config["graph_options"]["pressure"]["data_display_mode"] == "seconds":
-        self.ui.pressureLastXSecsRB.setChecked(True)
-    self.ui.pressureXSB.setValue(self.config["graph_options"]["pressure"]["X"])
-    self.ui.pressureThresholdList.addItems([str(marker) for marker in self.config["graph_options"]["pressure"]["thresholds"]])
+        self.config = {}
+
+    def load_config(self, config_path: str):
+        try:
+            with open(config_path) as config_file:
+                self.config = json.load(config_file)
+                # self.config_loaded.emit(self.config)
+        except Exception as e:
+            self.popup_ready.emit(f"Couldn't load config: {e}")
+            self.log_ready.emit()
     
-    if self.config["graph_options"]["temperature"]["data_display_mode"] == "points":
-        self.ui.temperatureLastXPointsRB.setChecked(True)
-    elif self.config["graph_options"]["temperature"]["data_display_mode"] == "seconds":
-        self.ui.temperatureLastXSecsRB.setChecked(True)
-    self.ui.temperatureXSB.setValue(self.config["graph_options"]["temperature"]["X"])
-    self.ui.temperatureThresholdList.addItems([str(marker) for marker in self.config["graph_options"]["temperature"]["thresholds"]])
-    
-    if self.config["graph_options"]["tank_mass"]["data_display_mode"] == "points":
-        self.ui.tankMassLastXPointsRB.setChecked(True)
-    elif self.config["graph_options"]["tank_mass"]["data_display_mode"] == "seconds":
-        self.ui.tankMassLastXSecsRB.setChecked(True)
-    self.ui.tankMassXSB.setValue(self.config["graph_options"]["tank_mass"]["X"])
-    self.ui.tankMassThresholdList.addItems([str(marker) for marker in self.config["graph_options"]["tank_mass"]["thresholds"]])
-    
-    if self.config["graph_options"]["engine_thrust"]["data_display_mode"] == "points":
-        self.ui.engineThrustLastXPointsRB.setChecked(True)
-    elif self.config["graph_options"]["engine_thrust"]["data_display_mode"] == "seconds":
-        self.ui.engineThrustLastXSecsRB.setChecked(True)
-    self.ui.engineThrustXSB.setValue(self.config["graph_options"]["engine_thrust"]["X"])
-    self.ui.engineThrustThresholdList.addItems([str(marker) for marker in self.config["graph_options"]["engine_thrust"]["thresholds"]])
+    def points_for_average_change_handler(self, value: float):
+        self.config["sensor_and_valve_options"]["points_used_for_average"] = value
+        
+    def pressure_data_display_change_handler(self, button: QRadioButton):
+        self.config["graph_options"]["pressure"]["data_display_mode"] = button.property("type")
+        for plot in ["p0", "p1", "p2", "p3", "p4", "p5"]:
+            self.plot_data[plot].data_display_mode = PlotDataDisplayMode[button.property("type")]
+
+    def pressure_x_val_change_handler(self, value: int):
+        for plot in ["p0", "p1", "p2", "p3", "p4", "p5"]:
+            self.plot_data[plot].x_val = value
+
+    def temperature_data_display_change_handler(self: "MainWindow", button: QRadioButton):
+        self.config["graph_options"]["temperature"]["data_display_mode"] = button.property("type")
+        for plot in ["t0", "t1", "t2", "t3"]:
+            self.plot_data[plot].data_display_mode = PlotDataDisplayMode[button.property("type")]
+
+    def temperature_x_val_change_handler(self, value: int):
+        for plot in ["t0", "t1", "t2", "t3"]:
+            self.plot_data[plot].x_val = value
+
+    def tank_mass_data_display_change_handler(self, button: QRadioButton):
+        self.config["graph_options"]["tank_mass"]["data_display_mode"] = button.property("type")
+        for plot in ["m0"]:
+            self.plot_data[plot].data_display_mode = PlotDataDisplayMode[button.property("type")]
+
+    def tank_mass_x_val_change_handler(self: "MainWindow", value: int):
+        for plot in ["m0"]:
+            self.plot_data[plot].x_val = value
+
+    def engine_thrust_data_display_change_handler(self, button: QRadioButton):
+        self.config["graph_options"]["engine_thrust"]["data_display_mode"] = button.property("type")
+        for plot in ["th0"]:
+            self.plot_data[plot].data_display_mode = PlotDataDisplayMode[button.property("type")]
+
+    def engine_thrust_x_val_change_handler(self: "MainWindow", value: int):
+        for plot in ["th0"]:
+            self.plot_data[plot].x_val = value
+
+    def add_pressure_threshold_handler(self: "MainWindow"):
+        try:
+            if self.ui.pressureThresholdList.currentRow() == -1:
+                new_marker = self.ui.pressureThresholdInput.text()
+                self.ui.pressureThresholdList.addItem(str(float(new_marker)))
+                self.ui.pressurePlot.addItem(InfiniteLine(float(new_marker), angle=0, pen=inf_line_pen))
+                self.ui.pressureThresholdInput.setText("")
+            else:
+                to_remove = filter(lambda item: isinstance(item, InfiniteLine) and item.pos().y() == float(self.ui.pressureThresholdList.currentItem().text()), self.ui.pressurePlot.items())
+                self.ui.pressurePlot.removeItem(list(to_remove)[0])
+                self.ui.pressureThresholdList.takeItem(self.ui.pressureThresholdList.currentRow())
+        except Exception as e:
+            self.display_popup(QMessageBox.Icon.Critical, "Action failed", f"Adding pressure threshold marker failed\n{str(e)}")
+
+    def add_temperature_threshold_handler(self: "MainWindow"):
+        try:
+            if self.ui.temperatureThresholdList.currentRow() == -1:
+                new_marker = self.ui.temperatureThresholdInput.text()
+                self.ui.temperatureThresholdList.addItem(str(float(new_marker)))
+                self.ui.temperaturePlot.addItem(InfiniteLine(float(new_marker), angle=0, pen=inf_line_pen))
+                self.ui.temperatureThresholdInput.setText("")
+            else:
+                self.ui.temperatureThresholdList.takeItem(self.ui.temperatureThresholdList.currentRow())
+        except Exception as e:
+            self.display_popup(QMessageBox.Icon.Critical, "Action failed", f"Adding temperature threshold marker failed\n{str(e)}")
+
+    def add_tank_mass_threshold_handler(self: "MainWindow"):
+        try:
+            if self.ui.tankMassThresholdList.currentRow() == -1:
+                new_marker = self.ui.tankMassThresholdInput.text()
+                self.ui.tankMassThresholdList.addItem(str(float(new_marker)))
+                self.ui.tankMassPlot.addItem(InfiniteLine(float(new_marker), angle=0, pen=inf_line_pen))
+                self.ui.tankMassThresholdInput.setText("")
+            else:
+                self.ui.tankMassThresholdList.takeItem(self.ui.tankMassThresholdList.currentRow())
+        except Exception as e:
+            self.display_popup(QMessageBox.Icon.Critical, "Action failed", f"Adding tank mass threshold marker failed\n{str(e)}")
+
+    def add_engine_thrust_threshold_handler(self: "MainWindow"):
+        try:
+            if self.ui.engineThrustThresholdList.currentRow() == -1:
+                new_marker = self.ui.engineThrustThresholdInput.text()
+                self.ui.engineThrustThresholdList.addItem(str(float(new_marker)))
+                self.ui.engineThrustPlot.addItem(InfiniteLine(float(new_marker), angle=0, pen=inf_line_pen))
+                self.ui.engineThrustThresholdInput.setText("")
+            else:
+                self.ui.engineThrustThresholdList.takeItem(self.ui.engineThrustThresholdList.currentRow())
+        except Exception as e:
+            self.display_popup(QMessageBox.Icon.Critical, "Action failed", f"Adding engine thrust threshold marker failed\n{str(e)}")
+
+    def add_default_open_valve_handler(self: "MainWindow"):
+        try:
+            if self.ui.defaultOpenValvesList.currentRow() == -1:
+                new_valve = self.ui.defaultOpenValvesInput.text()
+                self.config["sensor_and_valve_options"]["default_open_valves"].append(int(new_valve))
+                self.ui.defaultOpenValvesList.addItem(str(int(new_valve)))
+                self.ui.defaultOpenValvesInput.setText("")
+                self.init_actuator_valve_label()
+            else:
+                self.ui.defaultOpenValvesList.takeItem(self.ui.defaultOpenValvesList.currentRow())
+        except Exception as e:
+            self.display_popup(QMessageBox.Icon.Critical, "Action failed", f"Adding default open valve failed\n{str(e)}")
 
 def save_config(self: "MainWindow"):
     try:
@@ -118,100 +195,7 @@ def save_config(self: "MainWindow"):
         self.write_to_log("Could not save configuration")
         self.display_popup(QMessageBox.Icon.Warning, "Configuration save failed", "Could not save configuration")
 
-def points_for_average_change_handler(self: "MainWindow"):
-    self.points_used_for_average = int(self.ui.numPointsAverageInput.value())
 
-def add_default_open_valve_handler(self: "MainWindow"):
-    try:
-        if self.ui.defaultOpenValvesList.currentRow() == -1:
-            new_valve = self.ui.defaultOpenValvesInput.text()
-            self.config["sensor_and_valve_options"]["default_open_valves"].append(int(new_valve))
-            self.ui.defaultOpenValvesList.addItem(str(int(new_valve)))
-            self.ui.defaultOpenValvesInput.setText("")
-            self.init_actuator_valve_label()
-        else:
-            self.ui.defaultOpenValvesList.takeItem(self.ui.defaultOpenValvesList.currentRow())
-    except Exception as e:
-        self.display_popup(QMessageBox.Icon.Critical, "Action failed", f"Adding default open valve failed\n{str(e)}")
 
-def pressure_data_display_change_handler(self: "MainWindow", button: QPushButton):
-    for plot in ["p0", "p1", "p2", "p3", "p4", "p5"]:
-        self.plot_data[plot].data_display_mode = PlotDataDisplayMode[button.property("type")]
 
-def pressure_x_val_change_handler(self: "MainWindow", value: int):
-    for plot in ["p0", "p1", "p2", "p3", "p4", "p5"]:
-        self.plot_data[plot].x_val = value
 
-def temperature_data_display_change_handler(self: "MainWindow", button: QPushButton):
-    for plot in ["t0", "t1", "t2", "t3"]:
-        self.plot_data[plot].data_display_mode = PlotDataDisplayMode[button.property("type")]
-
-def temperature_x_val_change_handler(self: "MainWindow", value: int):
-    for plot in ["t0", "t1", "t2", "t3"]:
-        self.plot_data[plot].x_val = value
-
-def tank_mass_data_display_change_handler(self: "MainWindow", button: QPushButton):
-    for plot in ["m0"]:
-        self.plot_data[plot].data_display_mode = PlotDataDisplayMode[button.property("type")]
-
-def tank_mass_x_val_change_handler(self: "MainWindow", value: int):
-    for plot in ["m0"]:
-        self.plot_data[plot].x_val = value
-
-def engine_thrust_data_display_change_handler(self: "MainWindow", button: QPushButton):
-    for plot in ["th0"]:
-        self.plot_data[plot].data_display_mode = PlotDataDisplayMode[button.property("type")]
-
-def engine_thrust_x_val_change_handler(self: "MainWindow", value: int):
-    for plot in ["th0"]:
-        self.plot_data[plot].x_val = value
-
-def add_pressure_threshold_handler(self: "MainWindow"):
-    try:
-        if self.ui.pressureThresholdList.currentRow() == -1:
-            new_marker = self.ui.pressureThresholdInput.text()
-            self.ui.pressureThresholdList.addItem(str(float(new_marker)))
-            self.ui.pressurePlot.addItem(InfiniteLine(float(new_marker), angle=0, pen=inf_line_pen))
-            self.ui.pressureThresholdInput.setText("")
-        else:
-            to_remove = filter(lambda item: isinstance(item, InfiniteLine) and item.pos().y() == float(self.ui.pressureThresholdList.currentItem().text()), self.ui.pressurePlot.items())
-            self.ui.pressurePlot.removeItem(list(to_remove)[0])
-            self.ui.pressureThresholdList.takeItem(self.ui.pressureThresholdList.currentRow())
-    except Exception as e:
-        self.display_popup(QMessageBox.Icon.Critical, "Action failed", f"Adding pressure threshold marker failed\n{str(e)}")
-
-def add_temperature_threshold_handler(self: "MainWindow"):
-    try:
-        if self.ui.temperatureThresholdList.currentRow() == -1:
-            new_marker = self.ui.temperatureThresholdInput.text()
-            self.ui.temperatureThresholdList.addItem(str(float(new_marker)))
-            self.ui.temperaturePlot.addItem(InfiniteLine(float(new_marker), angle=0, pen=inf_line_pen))
-            self.ui.temperatureThresholdInput.setText("")
-        else:
-            self.ui.temperatureThresholdList.takeItem(self.ui.temperatureThresholdList.currentRow())
-    except Exception as e:
-        self.display_popup(QMessageBox.Icon.Critical, "Action failed", f"Adding temperature threshold marker failed\n{str(e)}")
-
-def add_tank_mass_threshold_handler(self: "MainWindow"):
-    try:
-        if self.ui.tankMassThresholdList.currentRow() == -1:
-            new_marker = self.ui.tankMassThresholdInput.text()
-            self.ui.tankMassThresholdList.addItem(str(float(new_marker)))
-            self.ui.tankMassPlot.addItem(InfiniteLine(float(new_marker), angle=0, pen=inf_line_pen))
-            self.ui.tankMassThresholdInput.setText("")
-        else:
-            self.ui.tankMassThresholdList.takeItem(self.ui.tankMassThresholdList.currentRow())
-    except Exception as e:
-        self.display_popup(QMessageBox.Icon.Critical, "Action failed", f"Adding tank mass threshold marker failed\n{str(e)}")
-
-def add_engine_thrust_threshold_handler(self: "MainWindow"):
-    try:
-        if self.ui.engineThrustThresholdList.currentRow() == -1:
-            new_marker = self.ui.engineThrustThresholdInput.text()
-            self.ui.engineThrustThresholdList.addItem(str(float(new_marker)))
-            self.ui.engineThrustPlot.addItem(InfiniteLine(float(new_marker), angle=0, pen=inf_line_pen))
-            self.ui.engineThrustThresholdInput.setText("")
-        else:
-            self.ui.engineThrustThresholdList.takeItem(self.ui.engineThrustThresholdList.currentRow())
-    except Exception as e:
-        self.display_popup(QMessageBox.Icon.Critical, "Action failed", f"Adding engine thrust threshold marker failed\n{str(e)}")
