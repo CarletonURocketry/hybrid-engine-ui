288f9a143a24e9935a0400e1912880bfe87f6995
Elias Hawa <elias.hawa2468@gmail.com>
2025-07-28 23:58:30 -0400

    Start using QFile and QDatastreams instead of reg python files

--- main_window/recording_and_playback.py
+++ main_window/recording_and_playback.py
@@ -7,8 +7,8 @@ Should only be imported by main_window.py
 import pathlib
 from io import BufferedWriter
 
-from PySide6.QtWidgets import QFileDialog
-from PySide6.QtCore import QDateTime, QObject, Slot, Signal
+from PySide6.QtWidgets import QWidget, QFileDialog
+from PySide6.QtCore import QDateTime, QObject, Slot, Signal, QFile, QDataStream, QIODevice
 
 import packet_spec
 
@@ -22,7 +22,8 @@ class PlaybackManager(QObject):
     def __init__(self):
         self.playback_ptr = 0
         self.buffered_writer = None
-        self.data_buffer = None
+        self.data_buffer: QFile = None
+        self.stream: QDataStream = None
         super().__init__()
 
     @Slot()
@@ -53,24 +54,20 @@ class PlaybackManager(QObject):
         # signal doesn't create one
         if file_path:
             self.log_ready.emit(f"Reading data from {file_path}")
-            with open(file_path, "rb") as file:
-                self.playback_ptr = 0
-                self.data_buffer = file.read()
-                self.playback_started.emit()
-            self.log_ready.emit("Data loaded")
+            self.data_buffer = QFile(file_path)
+            self.data_buffer.open(QIODevice.OpenModeFlag.ReadOnly)
+            self.stream = QDataStream(self.data_buffer)
+            self.playback_started.emit()
 
     @Slot()
     def playback_packet(self):
-        data_len = len(self.data_buffer)
-        if self.playback_ptr < data_len:
-            header = self.data_buffer[self.playback_ptr : self.playback_ptr + 2]
-            self.playback_ptr += 2
+        if self.stream.atEnd():
+            self.playback_ended.emit()
+        else:
+            header = self.stream.readRawData(2)
             data_header = packet_spec.parse_packet_header(header)
             message_bytes_length = packet_spec.packet_message_bytes_length(data_header)
-            message = self.data_buffer[self.playback_ptr : self.playback_ptr + message_bytes_length]
+            message = self.stream.readRawData(message_bytes_length)
             data_message = packet_spec.parse_packet_message(data_header, message)
             self.playback_ptr += message_bytes_length
             self.parsed_packet_ready.emit(data_header, data_message)
-        else:
-            self.playback_ended.emit()
-
